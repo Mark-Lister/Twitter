8c9caa8bbd09e4d72e2276ffd3cbfbcb8d303011
mark-lister <markjlister@yahoo.com>
2019-05-23 20:04:55 +0700

    Fin

--- index.js
+++ index.js
@@ -2,21 +2,61 @@
 var express = require('express');
 var Twit = require("twit");
 var config = require("./config");
+const socket = require("socket.io");
 var path = require('path');
+
 console.log("Starting...");
+
+
 var app = express();
-var T = new Twit(config);
-var tweets = [];
-var stream = T.stream('statuses/filter', { track: '#tradewar' });
-stream.on('tweet', function (tweet) {
-        console.log(tweet['created_at']);
-        tweets.push({name:tweet['text'],
-        time:tweet['created_at']});
-  })
+app.set("view engine", "pug");
+
+
 app.get('/', function (req, res) {
-        //res.sendFile(path.join(__dirname + '/index.html', {tweets:tweets})); 
-  res.send(tweets); 
+        res.sendFile(path.join(__dirname + '/index.html')); 
+  //res.send(tweets); 
 });
-app.listen(3000, function () {
+
+const server = app.listen(3000, function () {
    console.log('Started listening on port 3000!');
 });
+
+
+
+const io = socket.listen(server);
+io.on("connection", client => {
+  console.log("user connected");
+  io.sockets.emit("chat", tweets);
+  client.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+
+var T = new Twit(config);
+
+
+var stream = T.stream('statuses/filter', { track: 'tradewar' });
+
+let tweetsPerMin = {
+  date: new Date,
+  tweets: 0
+};
+
+var tweets = [];
+
+
+stream.on('tweet', function (tweet) {
+    tweetsPerMin.tweets = tweetsPerMin.tweets + 1;
+  });
+
+setInterval(function() {
+  tweets.push({
+      date: tweetsPerMin.date,
+      tweets: tweetsPerMin.tweets
+    });
+    io.sockets.emit("chat", tweets);
+    console.log('New time reseting count!');
+    tweetsPerMin.date = new Date;
+    tweetsPerMin.tweets = 0;
+}, 3600000);
